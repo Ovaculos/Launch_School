7a9a537876d0610553bba7a7aa051e25cce992fe
Mason Abbruzzese <masonderolo@gmail.com>
2021-10-22 15:34:45 -0600

    my_calc rubocop'd and pseudo_code

--- lesson_2/my_calculator.rb
+++ lesson_2/my_calculator.rb
@@ -1,15 +1,15 @@
-puts "Put in number 1."
-num_1 = gets.chomp.to_f
-puts "Put in number 2."
-num_2 = gets.chomp.to_f
-puts "What type of operation would you like? ( + / - / * / / )"
+puts 'Put in number 1.'
+num_one = gets.chomp.to_f
+puts 'Put in number 2.'
+num_two = gets.chomp.to_f
+puts 'What type of operation would you like? ( + / - / * / / )'
 operation = gets.chomp
 
 case operation
-when "+" then answer = num_1 + num_2
-when "-" then answer = num_1 - num_2
-when "*" then answer = num_1 * num_2
-when "/" then answer = num_1 / num_2
+when '+' then answer = num_one + num_two
+when '-' then answer = num_one - num_two
+when '*' then answer = num_one * num_two
+when '/' then answer = num_one / num_two
 end
 
 puts answer
---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,16 @@
+# sum of two integers
+# Define a method (sum) that takes two integers as arguments
+# Assign a result variable to the sum of the two integers
+# Puts result
+
+# a method that takes an array of strings, and returns a string that is all those strings concatenated together
+# Define a method (string_combo) that takes an array as an argument
+# Take the first string in the array, and set it to it's own string
+# Iteratively take each string after the first and add it to the first string
+# Puts the string
+
+# a method that takes an array of integers, and returns a new array with every other element
+# Define a method (other_integer) that takes an array as an argument
+# Set a counter to zero that increments every iteration of the array
+# When the counter is even, it will add the integer to a new array 
+# Puts the new array
